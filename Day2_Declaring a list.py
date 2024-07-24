ba0e27dc25b71d6d3777ea71aa3d79744bb476d3
Rajya Lakshmi Gopi Reddy <167053006+RajyaLakshmi1505@users.noreply.github.com>
2024-07-24 18:55:20 +0530

    Update Day2_Declaring a list.py

--- Day2_Declaring a list.py
+++ Day2_Declaring a list.py
@@ -1,15 +1,15 @@
 my_list=[1,2,3,4]
-#my_list.append(9)#append used to append to list
-#my_list.insert(3,9)#.insert(index,value)
-#print(len(my_list))
-#my_list.pop(0)#deletes element at the specified index
+my_list.append(9)#append used to append to list
+my_list.insert(3,9)#.insert(index,value)
+print(len(my_list))
+my_list.pop(0)#deletes element at the specified index
 my_list_2=[2,-5,65,-1,0,1,90,4]
-#new_list=my_list+my_list_2#By using '+', concatenates the list
-#new_list.pop()
-#print(*my_list)
-#print(*new_list)#* to removebraces and comas
-#print(new_list.count(4))
-#time complexity of quicksort is nlogn
+new_list=my_list+my_list_2#By using '+', concatenates the list
+new_list.pop()
+print(*my_list)
+print(*new_list)#* to removebraces and comas
+print(new_list.count(4))
+time complexity of quicksort is nlogn
 my_list_2.sort()#sorts the list,do sort operation on the original list and print the same list
 print(*my_list_2)#to print the sorted list
 
